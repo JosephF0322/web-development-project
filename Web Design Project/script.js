fd81ce790963c47037aeef5dd41cb339c547d1a9
JosephF0322 <122554773+JosephF0322@users.noreply.github.com>
2024-03-03 19:26:41 -0500

    Add files via upload

---
+++ Web Design Project/script.js
@@ -0,0 +1,92 @@
+// when you click on the open sidebar button  this sets the width of the sidebar to 250 and shrinks the main body by 250
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+// opposite of the above comment for when you close the sidebar
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+//when you scroll this event listener will trigger starting the reveal function
+window.addEventListener("scroll", reveal);
+  //reveals the document as you scroll
+  function reveal(){
+  var reveals = document.querySelectorAll('.reveal');
+
+  //for each selection with reveal in the name if that selection is on screen add active to the selection's name
+  // in the CSS file this selection is then translated to its default position (it is by default translated off-screen)
+  for(var i = 0; i < reveals.length; i++){
+    var windowHeight = window.innerHeight;
+    var revealTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 150;
+
+    if(revealTop < windowHeight - revealPoint){
+      reveals[i].classList.add('active');
+    } else {
+      reveals[i].classList.remove('active');
+    }
+  }
+}
+
+const form = document.getElementById('form');
+const fname = document.getElementById('fname');
+const lname = document.getElementById('lname')
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const fnameValue = fname.value.trim();
+    const lnameValue = lname.value.trim();
+    const emailValue = email.value.trim();
+
+    if(fnameValue === '') {
+        setError(fname, 'First Name is required');
+    } else {
+        setSuccess(fname);
+    }
+
+    if(lnameValue === '') {
+      setError(lname, 'Last Name is required');
+  } else {
+      setSuccess(lname);
+  }
+
+
+    if(emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
+    }
+};

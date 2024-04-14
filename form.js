a101d36ed00ce9e945bd6157e8bf591a74856459
JosephF0322 <122554773+JosephF0322@users.noreply.github.com>
2024-04-14 17:26:33 +0000

    added possible css functionality

--- form.js
+++ form.js
@@ -32,7 +32,8 @@ function createServer(req, res) {
 }
 */
 
-app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));// for parsing application/x-www-form-urlencoded
+app.use(express.static("public"));
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/Contact.html'); // send HTML file on GET request
